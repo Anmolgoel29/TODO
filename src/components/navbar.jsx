c3c97c84e7df358c315d95c362c01b654831c622
Anmo!Goe! <88969348+Anmolgoel29@users.noreply.github.com>
2024-08-05 02:25:07 +0530

    path issue res

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,17 +6,17 @@ function Navbar() {
   return (
     <div className="nav h-svh w-fit flex flex-col gap-6 p-3 fixed"  style={{minWidth:"78px"}}>
       <img
-        src="/ovals.png"
+        src="ovals.png"
         alt="ovals"
         className="w-8 p-3 box-content"
       />
       <img
-        src="/Logo.png"
+        src="Logo.png"
         alt="Logo"
         className="w-6 hover:bg-gray-800 rounded-full p-3 box-content"
       />
       <img
-        src="/sidebar.png"
+        src="sidebar.png"
         alt="sidebar icon"
         className="w-6 hover:bg-gray-800 rounded-full p-3 box-content"
       />
