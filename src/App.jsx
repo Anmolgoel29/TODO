7e10180d7ce1c66ed08174285dd590eabaad6818
Anmo!Goe! <88969348+Anmolgoel29@users.noreply.github.com>
2025-03-21 23:59:59 +0530

    fixed the zoom issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,9 @@ import Navbar from "./components/navbar";
 import Body from "./components/body";
 
 function App() {
-  function setZoomLevel(level) {
-    document.body.zoom(level)
-    // document.documentElement.style.transform = `scale(${level})`;
-    // document.documentElement.style.transformOrigin = "0 0"; // Adjust origin if necessary
-  }
 
   return (
-    <div className="app flex 2xl:h-svh pb-10" onLoad="setZoomLevel(.8)">
+    <div className="app flex 2xl:h-svh pb-10">
       <Navbar />
       <Body />
     </div>
